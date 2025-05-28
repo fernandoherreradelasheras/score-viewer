4a393f73a6adfa4684fd0ce7a06f87cb35079717
Fernando Herrera <fherrera@onirica.com>
2025-05-29 00:33:00 +0200

    update iframe code to the latest config format

--- iframe/ScoreViewerWrapper.tsx
+++ iframe/ScoreViewerWrapper.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
-import ScoreViewer, { ScoreItem, ScoreViewerConfig } from "../src/ScoreViewer";
-import React from "react";
-import { FacsimileItem } from "../src/types";
+import ScoreViewer from "../src/ScoreViewer";
+import { ScoreViewerConfig } from "../src/types/config";
 
 function ScoreViewerWrapper() {
 
@@ -12,34 +11,28 @@ function ScoreViewerWrapper() {
     const wrapUrl = (url:string) =>
         url.startsWith("http") || url.startsWith("/") ? url : `${parent}/${url}`
 
-
     const [config, setConfig] = useState<ScoreViewerConfig | null | undefined>()
 
     useEffect(() => {
         async function loadConfig() {
-            const response = await fetch(configUrl)
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`)
+            try {
+                const response = await fetch(configUrl)
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`)
+                }
+
+                const data = await response.json() as ScoreViewerConfig
+                data.settings.basePath = wrapUrl(data.settings.basePath)
+                data.settings.facsimileImagesPath = wrapUrl(data.settings.facsimileImagesPath)
+                setConfig(data)
+            } catch (error) {
+                console.error("Error loading config:", error)
             }
-            const data = await response.json() as ScoreViewerConfig
-            data.scores = data.scores.map((score: ScoreItem) => ({
-                ...score,
-                meiUrl: wrapUrl(score.meiUrl),
-                textUrl: score.textUrl ? wrapUrl(score.textUrl) : undefined,
-                audioUrl: score.audioUrl ? wrapUrl(score.audioUrl) : undefined,
-                facsimileItems: score.facsimileItems ? score.facsimileItems.map((item: FacsimileItem) => ({
-                    name: item.name,
-                    url: wrapUrl(item.url)
-                })) : undefined
-            }))
-            setConfig(data)
         }
         loadConfig()
     }, [])
 
     return config ? <ScoreViewer width="100%" height="95vh" config={config} /> : null
-
-
 }
 
 export default ScoreViewerWrapper;
