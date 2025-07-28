902ae87e51f7478a45848a622188082e4255f62e
Fernando Herrera <fherrera@onirica.com>
2025-07-28 19:16:57 +0200

    Move translations from backend to assets.

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,24 +1,28 @@
-// i18n.ts
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-xhr-backend'
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { SUPPORTED_LANGUAGES, LANGUAGE_SESSION_STORAGE_KEY } from './types/ui';
+import es from '../assets/locales/es/common.json';
+import en from '../assets/locales/en/common.json';
+
 
 const i18nextOptions = {
     order: ['sessionStorage', 'navigator'],
     lookupSessionStorage: LANGUAGE_SESSION_STORAGE_KEY,
     caches: ['sessionStorage'],
 }
+const resources = {
+  en: { common: en },
+  es: { common: es }
+};
 
-console.log(`Initializing i18next with options: ${JSON.stringify(i18nextOptions)}`);
 
 i18n
-  .use(Backend)
   .use(initReactI18next)
   .use(LanguageDetector)
   .init({
     supportedLngs: SUPPORTED_LANGUAGES.map(lang => lang.key),
+    resources,
     ...i18nextOptions,
     fallbackLng: 'en',
     debug: true,
@@ -26,8 +30,7 @@ i18n
     interpolation: {
       escapeValue: false
     }
-  });
+});
 
-  console.log(`inext initialized with languages: ${i18n.languages}`);
 
 export default i18n;
