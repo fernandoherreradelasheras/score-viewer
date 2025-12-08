8e8563f077e24c8d81e966f753528c0f9778c6b7
Fernando Herrera <fherrera@onirica.com>
2025-12-08 22:42:18 +0100

    update UI strings for split view selector

--- src/components/SplitViewSelector.tsx
+++ src/components/SplitViewSelector.tsx
@@ -14,8 +14,10 @@ export default function SplitViewSelector({
   introView,
   textView
 }: SplitViewSelectorProps) {
+
   const { t } = useTranslation("common");
   const activeSplitView = useStore.use.activeSplitView();
+  const splitViewOrientation = useStore.use.splitViewOrientation();
   const setActiveSplitView = useStore.use.setActiveSplitView();
 
   const selectorItems = useMemo(() => [
@@ -23,7 +25,12 @@ export default function SplitViewSelector({
     introView ? { label: t('tab.introduction'), value: 'intro' } : null,
     textView ? { label: t('tab.text'), value: 'text' } : null,
   ].filter(item => item != null)
-  ,[facsimileView, introView, textView, t]);
+    , [facsimileView, introView, textView, t]);
+
+  const splitViewContentSelectorLabel = useMemo(() =>
+    splitViewOrientation === "horizontal" ? t('heading.splitViewSelectorHorizontal') : t('heading.splitViewSelectorVertical')
+    , [splitViewOrientation, t]);
+
 
   const onActiveSplitViewChange = useCallback((value: string) => {
     setActiveSplitView(value);
@@ -35,7 +42,7 @@ export default function SplitViewSelector({
 
   return (
     <Space direction='horizontal' style={{ marginBottom: "10px", textAlign: "start", flex: "0" }}>
-      <Typography.Text style={{ marginLeft: "10px" }}>{t('heading.splitViewSelector')}:</Typography.Text>
+      <Typography.Text style={{ marginLeft: "10px" }}>{splitViewContentSelectorLabel}:</Typography.Text>
       <Select
         style={{ minWidth: "200px", marginRight: "10px" }}
         options={selectorItems}
